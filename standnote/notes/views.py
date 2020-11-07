371cedbb20622687f793dfbd28dcbdcf87db5190
Prateek Maheshwari <friskycodeur@gmail.com>
2020-11-07 21:41:38 +0530

    added post method

--- standnote/notes/views.py
+++ standnote/notes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Note
+from .serializers import NoteSerializer
+
+
+class NoteList(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
