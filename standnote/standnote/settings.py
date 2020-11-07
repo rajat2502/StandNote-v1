1f93ae010fd097ec2f80631fbd1227cf803e1c53
Prateek Maheshwari <friskycodeur@gmail.com>
2020-11-07 22:12:08 +0530

    Deploy Files added

--- standnote/standnote/settings.py
+++ standnote/standnote/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
